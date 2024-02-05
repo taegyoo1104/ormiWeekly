03aba64086c427d68380883a57a196441fbc787a
taegyoo1104 <taegyoo1104@gmail.com>
2024-02-05 15:37:37 +0900

    week2 // main에서 인터페이스 사용 수정

--- src/week2/Main.java
+++ src/week2/Main.java
@@ -5,18 +5,31 @@ import java.math.BigDecimal;
 public class Main {
 
     public static void main(String[] args) {
+        DeliveryChargeCalculator chargeCal;
 
         // 무게 11, 가격 10,000원 -> 기본배송비 10000원 + 추가 배송비 10000원 = 20000원
+        /*
         Beauty beauty = new Beauty("뷰티1", BigDecimal.valueOf(10000), 11.0);
         beauty.getDeliveryCharge();
+        */
+        chargeCal = new Beauty("뷰티1", BigDecimal.valueOf(10000), 11.0);
+        chargeCal.getDeliveryCharge();
 
         // 무게 2, 가격 50,000원 -> 기본 배송비 1000원 + 추가 배송비 (-1000원) = 0원
+        /*
         Grocery grocery = new Grocery("식료품1", BigDecimal.valueOf(50000), 2.0);
         grocery.getDeliveryCharge();
+         */
+        chargeCal = new Grocery("식료품1", BigDecimal.valueOf(50000), 2.0);
+        chargeCal.getDeliveryCharge();
 
         // 무게 130, 가격 1,300,000원 ->  가격 130만 이상, 배송비 0원
+        /*
         LargeAppliance largeAppliance = new LargeAppliance("가전1", BigDecimal.valueOf(1300000), 130.0);
         largeAppliance.getDeliveryCharge();
+         */
+        chargeCal = new LargeAppliance("가전1", BigDecimal.valueOf(1300000), 130.0);
+        chargeCal.getDeliveryCharge();
 
         /*
         OUTPUT
